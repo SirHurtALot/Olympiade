f69c30614dda04d35fe32e493bb20ea199f07568
SirHurtALot <chrissi_stalker@freenet.de>
2013-09-23 23:01:01 +0200

    Update main.java

--- Olympiade/src/main.java
+++ Olympiade/src/main.java
@@ -1,4 +1,3 @@
-
 public class main {
 
 	/**
@@ -8,6 +7,7 @@ public class main {
 		// Bla bla bla 
 		//Hallo Chris
 		// bitte schreib hier doch was dazu mit "//" (Kommentar)
+		//Ja klar ich weiß das noch von Java ich hab ja schließlich PROgrammierskills :'D
 
 	}
 
