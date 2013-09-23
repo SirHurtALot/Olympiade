53ba04dafcecbb88f7af3871b2a9e55286318539
mondeera <katyb@gmx.net>
2013-09-23 14:16:14 +0200

    Test

---
+++ Olympiade/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Bla bla bla 
+		//Hallo Chris
+		// bitte schreib hier doch was dazu mit "//" (Kommentar)
+
+	}
+
+}
