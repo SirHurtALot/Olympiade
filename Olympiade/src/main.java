1fbe0b94f5952df6c58cf3af8a220dd4aae3ebf3
mondeera <katyb@gmx.net>
2013-09-24 14:34:43 +0200

    quadratzahlengenerator

--- Olympiade/src/main.java
+++ Olympiade/src/main.java
@@ -5,11 +5,30 @@
 	 * @param args
 	 */
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		
 		// Das ist eine Schleife
 		for(int i = 1; i < 10; i++){
 			System.out.println(quadratZahl(i));
 		}
+=======
+		// Bla bla bla 
+		//Hallo Chris
+		// bitte schreib hier doch was dazu mit "//" (Kommentar)
+
+		//Ich hab übelste PROgrammierskills in Java!
+		//HERPADERPA
+		
+		//toll das dus wei�t!
+		
+		//Test test
+
+		//Ja klar ich weiß das noch von Java ich hab ja schließlich PROgrammierskills :'D
+		
+		
+		
+		//KAAAAAAAAAAAAAAAATYYYYY was geheeeeet aaaaaabeee? *alles kill*
+>>>>>>> 6d060eadaa34b5839196f0d9589e5ec23c824bc7
 
 	}
 
